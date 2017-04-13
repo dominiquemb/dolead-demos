176f0599e77a3a8ee6da14f70c249e8d5ee4b510
Dominique Moreno-Baltierra <conejoplata@gmail.com>
2017-04-13 10:27:47 +0000

    Functional!

--- js/dolead.js
+++ js/dolead.js
@@ -1,19 +1,30 @@
 $(document).ready(function() {
 	var multipleToggle = function(elem, max) {
+		var i = 0;
+		var otherButtonIndexes = [];
 		var activeButtons = elem.closest('.multiple-toggle').find('.btn.active').not('.skip');
-console.log(activeButtons.length);
-		if (activeButtons.length == max) {
-console.log('max');
-			var toBeUnselected = Math.floor(Math.random() * (max+1));
-console.log(toBeUnselected);
-			activeButtons.eq(toBeUnselected).removeClass('skip active btn-primary').addClass('btn-default');
+		activeButtons.each(function(index, val) {
+			otherButtonIndexes.push(parseInt($(val).data('index')));
+		});
+		if (otherButtonIndexes.length == max) {
+			var rand = Math.floor(Math.random() * (otherButtonIndexes.length));
+			var toBeUnselected = otherButtonIndexes[rand];
+			activeButtons.each(function(i, button) {
+				if (parseInt($(button).data('index')) == toBeUnselected) {
+					$(button).removeClass('skip active btn-primary').addClass('btn-default');
+				}
+			});
 		}
 		elem.removeClass('btn-default skip').addClass('btn-primary active');
 	};
 
 	$('.btn').click(function(evt) {
 		evt.preventDefault();
-		$(this).addClass('skip');
-		multipleToggle($(this), 2);
+		if ($(this).hasClass('btn-primary')) {
+			$(this).removeClass('btn-primary active skip').addClass('btn-default');
+		} else {
+			$(this).addClass('skip');
+			multipleToggle($(this), 2);
+		}
 	});
 });
