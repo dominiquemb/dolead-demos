c3876d5319be568f6280baef5c11fa7c85ced14a
Dominique Moreno-Baltierra <conejoplata@gmail.com>
2017-04-13 12:51:12 +0000

    Fetch data and re-render

--- js/dolead.js
+++ js/dolead.js
@@ -1,4 +1,6 @@
 $(document).ready(function() {
+	var mockData = { 0: true, 1: false, 2: true };
+
 	var multipleToggle = function(elem, max) {
 		var i = 0;
 		var otherButtonIndexes = [];
@@ -18,6 +20,30 @@ $(document).ready(function() {
 		elem.removeClass('btn-default skip').addClass('btn-primary active');
 	};
 
+	var updateUI = function() {
+		$('.multiple-toggle .btn').each(function(i, v) {
+			var active = mockData[i];
+			if (active) {
+				$(v).addClass('btn-primary active').removeClass('btn-default');
+			} 
+			else {
+				$(v).removeClass('btn-primary active').addClass('btn-default');
+			}
+		});
+	};
+
+	var fetchData = function() {
+		// just for demo purposes
+		mockData = {0: true, 1: true, 2: true};	
+
+		// simulating delay
+		setTimeout(function() {
+			updateUI();
+		}, 100);
+	};
+
+	var timer = false;
+
 	$('.btn').click(function(evt) {
 		evt.preventDefault();
 		if ($(this).hasClass('btn-primary')) {
@@ -26,5 +52,11 @@ $(document).ready(function() {
 			$(this).addClass('skip');
 			multipleToggle($(this), 2);
 		}
+
+		clearTimeout(timer);
+		timer = setTimeout(function() {
+			fetchData();
+		}, 500);
 	});
+
 });
